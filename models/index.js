4dfe289f11412d0542f9a8f588b699197658aa56
Thomas <malifarge.thomas9@gmail.com>
2024-01-29 06:31:54 +0100

    jawDb

--- models/index.js
+++ models/index.js
@@ -1,9 +1,9 @@
 const {Sequelize} = require("sequelize")
 
-const {DB_NAME,DB_USERNAME,DB_PASSWORD,DB_HOST} = process.env
+const {DB_NAME,DB_USERNAME,DB_PASSWORD,JAWSDB_URL} = process.env
 
 const sequelize = new Sequelize(DB_NAME,DB_USERNAME,DB_PASSWORD,{
-    host: DB_HOST,
+    host: JAWSDB_URL,
     dialect: "mysql",
     logging:false
 })
