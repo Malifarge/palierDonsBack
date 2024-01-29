8ee39d85e3c1e6356f7ea8303f1cd6d2c2c15e53
Thomas <malifarge.thomas9@gmail.com>
2024-01-29 06:44:52 +0100

    test avec jaw

--- models/index.js
+++ models/index.js
@@ -1,34 +1,64 @@
 const {Sequelize} = require("sequelize")
 
-const {DB_NAME,DB_USERNAME,DB_PASSWORD,JAWSDB_URL} = process.env
+const {DB_NAME,DB_USERNAME,DB_PASSWORD,DB_HOST,JAWSDB_URL} = process.env
 
-const sequelize = new Sequelize(DB_NAME,DB_USERNAME,DB_PASSWORD,{
-    host: JAWSDB_URL,
-    dialect: "mysql",
-    logging:false
-})
+if (JAWSDB_URL){
+    const sequelize = new Sequelize(JAWSDB_URL)
+    const connectDb = async () =>{
+        try{
+            await sequelize.authenticate()
+            console.log(`Connected to db ${sequelize.config.database}`);
+        }catch(e){
+            console.log(e);
+        }
+    }
+    
+    connectDb()
+    
+    const Users = require('./users')(sequelize)
+    const Paliers = require("./palier")(sequelize)
+    
+    Users.hasMany(Paliers)
+    Paliers.belongsTo(Users)
+    
+    sequelize.sync({alter:true})
+    
+    module.exports = {
+        sequelize,
+        Users,
+        Paliers
+    }
 
-const connectDb = async () =>{
-    try{
-        await sequelize.authenticate()
-        console.log(`Connected to db ${sequelize.config.database}`);
-    }catch(e){
-        console.log(e);
+}else{
+    const sequelize = new Sequelize(DB_NAME,DB_USERNAME,DB_PASSWORD,{
+        host: DB_HOST,
+        dialect: "mysql",
+        logging:false
+    })
+
+    const connectDb = async () =>{
+        try{
+            await sequelize.authenticate()
+            console.log(`Connected to db ${sequelize.config.database}`);
+        }catch(e){
+            console.log(e);
+        }
+    }
+    
+    connectDb()
+    
+    const Users = require('./users')(sequelize)
+    const Paliers = require("./palier")(sequelize)
+    
+    Users.hasMany(Paliers)
+    Paliers.belongsTo(Users)
+    
+    sequelize.sync({alter:true})
+    
+    module.exports = {
+        sequelize,
+        Users,
+        Paliers
     }
 }
 
-connectDb()
-
-const Users = require('./users')(sequelize)
-const Paliers = require("./palier")(sequelize)
-
-Users.hasMany(Paliers)
-Paliers.belongsTo(Users)
-
-sequelize.sync({alter:true})
-
-module.exports = {
-    sequelize,
-    Users,
-    Paliers
-}
