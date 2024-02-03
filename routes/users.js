78b4b0b1a310904f3271ee329ae08321ea6d739d
Thomas <malifarge.thomas9@gmail.com>
2024-02-03 08:14:56 +0100

    get users list

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,19 @@ const express = require('express')
 const app = express()
 const passport = require('../config/passport')
 const { userExist } = require('../middlewares/user')
+const { Users } = require('../models')
 
 app.get('/me',passport.authenticate('jwt'),(req,res)=>{
     res.json(req.user)
+})
+
+app.get('/user',async (req,res)=>{
+    try{
+        const streams = await Users.findAll({})
+        res.json(streams)
+    }catch(e){
+        res.status(500).json("Internal server error")
+    }
 })
 
 app.get ('/user/:user_Id',userExist,(req,res)=>{
